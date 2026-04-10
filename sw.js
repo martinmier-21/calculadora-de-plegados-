b36b9b09d4d3c95b9bd0e9440b93ce8743844195
martinmier-21 <64104597+martinmier-21@users.noreply.github.com>
2026-04-09 21:25:00 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,20 @@
-﻿const CACHE_NAME = 's11-v1';
-const ASSETS = [
+const CACHE_NAME = 'plegado-v1';
+const urlsToCache = [
   './',
-  './tu_archivo.html',
+  './index.html',
   './manifest.json'
 ];
 
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
